1abc5a32ceec866f4355fdf791611d372908ee1b
Otso Rasimus <orasimus@gmail.com>
2017-11-24 21:04:55 +0200

    Add logo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Matilla on enemmän rahaa kuin sinulla!'));
+app.use(express.static('static'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
