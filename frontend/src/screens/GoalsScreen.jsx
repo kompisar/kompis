50f5193fef7b2f53acabe7744495d25c02dc602b
Aarni Koskela <akx@iki.fi>
2017-11-26 01:01:09 +0200

    doot

--- frontend/src/screens/GoalsScreen.jsx
+++ frontend/src/screens/GoalsScreen.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Box, Flex } from 'reflexbox';
 import { withRouter } from 'react-router-dom';
 import MainNav from '../components/MainNav';
+import doot from '../img/doot.gif';
 
 const GoalsScreen = withRouter(({ history }) => (
-  <Flex flex auto column className="bg-white">
+  <Flex flex auto column className="bg-dark">
     <MainNav active="goals" />
     <Box style={{ margin: '2em', textAlign: 'center' }}>
-      ghouls and skullingtons???
+      <p>ghouls and skullingtons???</p>
+      <img src={doot} style={{ width: '100%' }} />
     </Box>
   </Flex>
 ));
