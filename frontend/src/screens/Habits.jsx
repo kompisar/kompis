38628459c049b2ab2b8f18a6022c13936ba78168
Otso Rasimus <orasimus@gmail.com>
2017-11-25 20:42:21 +0200

    Add font-awsum

--- frontend/src/screens/Habits.jsx
+++ frontend/src/screens/Habits.jsx
@@ -1,34 +1,24 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import { Box } from 'reflexbox';
+import { Box, Flex } from 'reflexbox';
 import { spends } from '../config';
 import { formatEUR } from '../utils';
 import { typeColors, typeTitles } from '../consts';
+import FontAwesome from 'react-fontawesome';
 import Backbloder from '../components/Backploder';
+import KompisBars from '../components/KompisBars';
 
 class HabitBox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      explode: false,
-    };
-  }
-
   render() {
-    const { type, history } = this.props;
-    const clickHandler = () => {
-      this.setState({ explode: true });
-      setTimeout(() => history.push(`/spending/${type}`), 300);
-    };
-
+    const { type, explode, history } = this.props;
     const color = typeColors[type];
     const title = typeTitles[type];
     const typeSpends = spends.filter(s => s.type === this.props.type);
     const total = typeSpends.reduce((acc, s) => acc + s.value, 0);
     return (
-      <Backbloder explode={this.state.explode}>
-        <div className={`habit-box habit-box-${color}`} onClick={clickHandler}>
+      <Backbloder explode={this.props.explode}>
+        <div className={`habit-box habit-box-${color}`}>
           <div className="title">{title}</div>
           <div className="amount-ctr">
             <span className="amount">{formatEUR(total)}</span>
@@ -43,13 +33,29 @@ class HabitBox extends React.Component {
 const HabeebBox = withRouter(HabitBox);
 
 class HabitsScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      explode: false,
+    }
+  }
+
   render() {
+    const clickHandler = () => {
+      this.setState({ explode: true });
+      setTimeout(() => history.push(`/spending/nonessential`), 300);
+    };
+    
     return (
-      <Box auto className="bg-white-logo">
-        <HabeebBox type="essential" />
-        <HabeebBox type="nonessential" />
-        <HabeebBox type="saving" />
-      </Box>
+      <Box auto className="bg-white-logo" style={{'padding-top': '5em'}}>
+        <HabeebBox type="essential" explode={false} />
+        <HabeebBox type="nonessential" explode={this.state.explode} />
+        <HabeebBox type="saving" explode={false} />
+        <Flex justify="space-between" style={{margin: '1em', 'margin-top': '3em', 'font-size': '18pt'}} onClick={clickHandler}>
+          <Box>Where could we save</Box>
+          <Box><FontAwesome name="arrow-right" className="habit-text" /></Box>
+        </Flex>
+      </Box >
     );
   }
 }
