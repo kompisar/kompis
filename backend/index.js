d1c567006d0e8c2a797bb0b7c9eaca875870613a
Otso Rasimus <orasimus@gmail.com>
2017-11-25 13:13:35 +0200

    Add an endpoint for fetching user's transaction data

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,22 @@
+const fs = require('fs');
+const promisify = require('util').promisify;
+const readFile = promisify(fs.readFile)
+
 const express = require('express');
 const app = express();
 
+app.get('/:id/transactions', async (req, res) => {
+  try {
+    const data = await readFile('../nordea-responses/account-txns.json');
+    const json = JSON.parse(data);
+    res.json(json);
+  } catch(e) {
+    res.status(500);
+    res.json(e);
+  }
+});
+
 app.use(express.static('static'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+const port = 3000;
+app.listen(port, () => console.log(`GOLD-JACK backend running on port ${port}`));
