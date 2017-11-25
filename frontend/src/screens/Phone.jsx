307f46914659e837865f342756758e0ebae72104
Ruksi <me@ruk.si>
2017-11-25 16:02:03 +0200

    Stylize notification header a bit better

--- frontend/src/screens/Phone.jsx
+++ frontend/src/screens/Phone.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
+import {Flex, Box} from 'reflexbox'
 import {ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 
 export default class Phone extends React.Component {
 
-  toastId = null;
-
   notify = () => {
-    if (!toast.isActive(this.toastId)) {
-      this.toastId = toast(
-        <div>
-          <div>
-            <div>Kompis</div>
-            <div>1m ago</div>
-          </div>
-          <div>
-            You have brought beer with $7.8 tonight, want to save that much?
-          </div>
-        </div>,
-        { closeButton: false }
-      )
-    }
+    toast(
+      <div>
+        <Flex>
+          <Box w={1 / 2} style={{textAlign: 'left'}}>Kompis</Box>
+          <Box w={1 / 2} style={{textAlign: 'right'}}>2m ago</Box>
+        </Flex>
+        <div style={{marginTop: '2vh'}}>
+          You have brought beer with $7.8 tonight, want to save that much?
+        </div>
+      </div>,
+      {closeButton: false}
+    )
   };
 
   render() {
