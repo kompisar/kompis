429ac59bd4014daf612606845a9c98a89a50469b
Aarni Koskela <akx@iki.fi>
2017-11-25 23:34:43 +0200

    Animate that goal ish

--- frontend/src/screens/Goals.jsx
+++ frontend/src/screens/Goals.jsx
@@ -6,22 +6,47 @@ import Imploder from '../components/Imploder';
 import goals from '../goals';
 import DetailCard, { DetailCardButton } from '../components/DetailCard';
 
+const GoalCardAndTitle = ({ goal, onDecision, fadeOut }) => (
+  <div className={fadeOut ? 'animated-half-second fadeOut' : 'animated-half-second fadeIn'}>
+    <div className="goal-overtext">
+      In the next {goal.timeframe}, I'd like to...
+    </div>
+    <DetailCard
+      image={goal.image}
+      title={goal.title}
+      value={goal.value}
+      actions={[
+        (<DetailCardButton icon="fa-close" text="No" key="no" onClick={() => onDecision(goal, 'no')} />),
+        (<DetailCardButton icon="fa-check" text="Yes" key="yes" onClick={() => onDecision(goal, 'yes')} />),
+      ]}
+    />
+  </div>
+);
+
 class GoalsScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      fadeOut: false,
       goalIndex: 0,
     };
   }
 
   onDecision = (goal, decision) => {
     goal.decision = decision;
-    const nextIndex = this.state.goalIndex + 1;
-    if (nextIndex >= goals.length) {
-      this.props.history.push('/onboarding-done');
-      return;
-    }
-    this.setState({ goalIndex: nextIndex });
+    this.setState(
+      { fadeOut: true },
+      () => {
+        setTimeout(() => {
+          const nextIndex = this.state.goalIndex + 1;
+          if (nextIndex >= goals.length) {
+            this.props.history.push('/onboarding-done');
+            return;
+          }
+          this.setState({ goalIndex: nextIndex, fadeOut: false });
+        }, 550);
+      },
+    );
   };
 
   render() {
@@ -29,18 +54,7 @@ class GoalsScreen extends React.Component {
     return (
       <Box auto flex className="bg-green-logo-light" style={{ paddingTop: '5em' }} column>
         <Imploder circleClassName="red-imploder" />
-        <div className="goal-overtext">
-          In the next {goal.timeframe}, I'd like to...
-        </div>
-        <DetailCard
-          image={goal.image}
-          title={goal.title}
-          value={goal.value}
-          actions={[
-            (<DetailCardButton icon="fa-close" text="No" key="no" onClick={() => this.onDecision(goal, 'no')} />),
-            (<DetailCardButton icon="fa-check" text="Yes" key="yes" onClick={() => this.onDecision(goal, 'yes')} />),
-          ]}
-        />
+        <GoalCardAndTitle fadeOut={this.state.fadeOut} goal={goal} onDecision={this.onDecision} />
       </Box>
     );
   }
