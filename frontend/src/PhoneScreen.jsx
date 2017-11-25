731a2ba9b496191821fc364183d3e169b8d5b598
Ruksi <me@ruk.si>
2017-11-25 15:35:44 +0200

    Add first prototype of notification demo 1

---
+++ frontend/src/PhoneScreen.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+
+export default class PhoneScreen extends React.Component {
+
+  toastId = null;
+
+  notify = () => {
+    if (!toast.isActive(this.toastId)) {
+      this.toastId = toast(
+        <div>
+          <div>
+            <div>Kompis</div>
+            <div>1m ago</div>
+          </div>
+          <div>
+            You have brought beer with $7.8 tonight, want to save that much?
+          </div>
+        </div>,
+        { closeButton: false }
+      )
+    }
+  };
+
+  render() {
+    return (
+      <main className="lock-screen-bg">
+        <button onClick={this.notify}>DEMO 1</button>
+        <ToastContainer
+          position="top-center"
+          autoClose={9999999999999}
+          hideProgressBar={true}
+          newestOnTop={false}
+        />
+      </main>
+    );
+  }
+
+}
