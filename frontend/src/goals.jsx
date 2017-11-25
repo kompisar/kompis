9cc5354fa633556dddb8fb420f39c3169ae8a5c0
Aarni Koskela <akx@iki.fi>
2017-11-25 23:01:46 +0200

    Little stubbings of Goals

---
+++ frontend/src/goals.jsx
@@ -0,0 +1,17 @@
+import vacationImage from './img/vacaycay.gif';
+import houseImage from './img/house.gif';
+
+export default [
+  {
+    timeframe: '20 years',
+    title: 'own my own house',
+    value: '€150K–250K',
+    image: houseImage,
+  },
+  {
+    timeframe: 'the next 12 months',
+    title: 'take a beach vacation',
+    value: '€1,500-2,500',
+    image: vacationImage,
+  },
+];
--- frontend/src/screens/Goals.jsx
+++ frontend/src/screens/Goals.jsx
@@ -2,14 +2,33 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { Box } from 'reflexbox';
-import ArrowCTA from '../components/ArrowCTA';
 import Imploder from '../components/Imploder';
+import goals from '../goals';
+import DetailCard from '../components/DetailCard';
 
 class GoalsScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      goalIndex: 0,
+    };
+
+  }
+
   render() {
+    const goal = goals[this.state.goalIndex];
     return (
-      <Box auto flex className="bg-white-logo-dark" style={{ paddingTop: '5em' }} column>
+      <Box auto flex className="bg-green-logo-light" style={{ paddingTop: '5em' }} column>
         <Imploder circleClassName="red-imploder" />
+        <div className="goal-overtext">
+          In the next {goal.timeframe}, I'd like to...
+        </div>
+        <DetailCard
+          image={goal.image}
+          title={goal.title}
+          value={goal.value}
+        />
+
       </Box>
     );
   }
