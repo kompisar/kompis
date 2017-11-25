0c5361b9ae4a97a5cabdff3e2923a210ea49a218
Aarni Koskela <akx@iki.fi>
2017-11-25 22:46:43 +0200

    Reformat backploder

--- frontend/src/components/Backploder.jsx
+++ frontend/src/components/Backploder.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 export default class Backploder extends React.Component {
-    render() {
-        return (
-            <div className={this.props.explode ? 'ball-explode' : ''}>
-                {this.props.children}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className={this.props.explode ? 'ball-explode' : ''}>
+        {this.props.children}
+      </div>
+    );
+  }
 };
