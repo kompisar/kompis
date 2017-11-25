39d64b1e57e1fb625a452eb59a928ec32ecebd4c
Aarni Koskela <akx@iki.fi>
2017-11-25 16:59:11 +0200

    Restyle more

--- frontend/src/screens/AnalyzingScreen.jsx
+++ frontend/src/screens/AnalyzingScreen.jsx
@@ -5,6 +5,7 @@ import { withRouter } from 'react-router-dom';
 import CircularProgressbar from 'react-circular-progressbar';
 import { sample, truncate } from 'lodash';
 import payees from '../data/payees';
+import KompisBars from '../components/KompisBars';
 
 
 class AnalyzingScreen extends React.Component {
@@ -25,18 +26,21 @@ class AnalyzingScreen extends React.Component {
   }
 
   tick() {
-    const newProgress = this.state.progress + (Math.random() * 2);
-    this.setState({
-      progress: newProgress,
-      lastPayee: sample(payees),
-    });
+    const progress = this.state.progress;
+    if (progress < 100) {
+      const newProgress = progress + (Math.random() * 2);
+      this.setState({
+        progress: newProgress,
+        lastPayee: sample(payees),
+      });
+    }
   }
 
   render() {
     const { progress, lastPayee } = this.state;
     const { history } = this.props;
     let content = null;
-    if (progress < 100) {
+    if (lastPayee && progress < 100) {
       content = (
         <div style={{ fontSize: '20pt' }}>
           <div style={{ margin: '1em' }}>Just a moment, taking a look at your spending habits...</div>
@@ -47,17 +51,17 @@ class AnalyzingScreen extends React.Component {
     } else {
       content = (
         <div
-          style={{ fontSize: '28pt', margin: '1em' }}
+          style={{ fontSize: '24pt', margin: '1em' }}
           className="animated fadeIn"
           onClick={() => history.push('/result')}
         >
-          <p>Okay, got it!</p>
-          Let's take a look at how you spend your hard-earned cash!
+          <KompisBars style={{ width: '35vw', display: 'block', margin: 'auto' }} />
+          Let's look at how you spend your hard-earned money
         </div>
       );
     }
     return (
-      <Flex flex auto align="center" justify="center" column className="bg-purple">
+      <Flex flex auto align="center" justify="center" column className="bg-white-logo">
         <div style={{ textAlign: 'center' }}>
           {content}
         </div>
--- frontend/src/screens/Welcome.jsx
+++ frontend/src/screens/Welcome.jsx
@@ -5,7 +5,7 @@ import { withRouter } from 'react-router-dom';
 import KompisBars from '../components/KompisBars';
 
 const WelcomeScreen = withRouter(({ history }) => (
-  <Flex flex auto align="center" justify="center" column className="bg-white animated fadeIn" onClick={() => history.push('/analyze')}>
+  <Flex flex auto align="center" justify="center" column className="bg-white-logo animated fadeIn" onClick={() => history.push('/analyze')}>
     <div style={{ textAlign: 'center', fontSize: '48pt' }}>
       <KompisBars style={{ width: '35vw', display: 'block', margin: 'auto' }} />
       Welcome
