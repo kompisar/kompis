cd6dc0a55c9808c4522921c2167d5477feeff6bb
Otso Rasimus <orasimus@gmail.com>
2017-11-25 15:11:43 +0200

    Make css animation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { Menu } from 'semantic-ui-react';
+import animatecss from 'animate.css';
 
 export default class App extends React.Component {
   render() {
@@ -13,7 +14,7 @@ export default class App extends React.Component {
             content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
           />
         </Helmet>
-        <Menu color="blue" inverted widths={3}>
+        <Menu className="animated infinite bounce" color="blue" inverted widths={3}>
           <Menu.Item name="home" active={activeItem === 'home'} onClick={this.handleItemClick} />
           <Menu.Item name="saving" active={activeItem === 'saving'} onClick={this.handleItemClick} />
           <Menu.Item name="friends" active={activeItem === 'friends'} onClick={this.handleItemClick} />
