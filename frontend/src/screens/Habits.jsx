77610d59e4a8e887d2288f5bf6dda3d29e246ed9
Aarni Koskela <akx@iki.fi>
2017-11-25 21:11:29 +0200

    Remangle the hobbit boxes so all are clickable

--- frontend/src/screens/Habits.jsx
+++ frontend/src/screens/Habits.jsx
@@ -7,18 +7,34 @@ import { formatEUR } from '../utils';
 import { typeColors, typeTitles } from '../consts';
 import FontAwesome from 'react-fontawesome';
 import Backbloder from '../components/Backploder';
-import KompisBars from '../components/KompisBars';
 
-class HabitBox extends React.Component {
+class HabeebBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { explode: false };
+  }
+
+  handleClick = () => {
+    const { type, onNavigate } = this.props;
+    this.setState(
+      { explode: true },
+      () => {
+        setTimeout(() => {
+          onNavigate(type);
+        }, 300);
+      },
+    );
+  };
+
   render() {
-    const { type, explode } = this.props;
+    const { type } = this.props;
     const color = typeColors[type];
     const title = typeTitles[type];
     const typeSpends = spends.filter(s => s.type === this.props.type);
     const total = typeSpends.reduce((acc, s) => acc + s.value, 0);
     return (
-      <Backbloder explode={this.props.explode}>
-        <div className={`habit-box habit-box-${color}`}>
+      <Backbloder explode={this.state.explode}>
+        <div className={`habit-box habit-box-${color}`} onClick={this.handleClick}>
           <div className="title">{title}</div>
           <div className="amount-ctr">
             <span className="amount">{formatEUR(total)}</span>
@@ -28,39 +44,45 @@ class HabitBox extends React.Component {
       </Backbloder>
     );
   }
-};
-
-const HabeebBox = withRouter(HabitBox);
+}
 
 class HabitsScreen extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      explode: false,
-    }
-  }
+  handleNavigate = (type) => {
+    const { history } = this.props;
+    history.push(`/spending/${type}`);
+  };
 
   render() {
-    const { history } = this.props;
     const clickHandler = () => {
-      this.setState({ explode: true });
-      setTimeout(() => history.push(`/spending/nonessential`), 300);
+      this.nonessentialBox.handleClick();
     };
 
+    const whereCouldWeSaveBar = (
+      <div>
+        <Flex
+          justify="space-between"
+          style={{ margin: '1em', marginTop: '3em', fontSize: '18pt' }}
+          onClick={clickHandler}
+        >
+          <Box>Where could we save</Box>
+          <Box><FontAwesome name="arrow-right" className="habit-text" /></Box>
+        </Flex>
+      </div>
+    );
+
     return (
-      <Box auto className="bg-white-logo" style={{ 'padding-top': '5em' }}>
-        <HabeebBox type="essential" explode={false} />
-        <HabeebBox type="nonessential" explode={this.state.explode} />
-        <HabeebBox type="saving" explode={false} />
-        {this.state.explode ? null : (
-          <div>
-            <Flex justify="space-between" style={{ margin: '1em', 'margin-top': '3em', 'font-size': '18pt' }} onClick={clickHandler}>
-              <Box>Where could we save</Box>
-              <Box><FontAwesome name="arrow-right" className="habit-text" /></Box>
-            </Flex>
-          </div>
-        )}
-      </Box >
+      <Box auto className="bg-white-logo" style={{ paddingTop: '5em' }}>
+        <HabeebBox type="essential" onNavigate={this.handleNavigate} />
+        <HabeebBox
+          type="nonessential"
+          onNavigate={this.handleNavigate}
+          ref={(box) => {
+            this.nonessentialBox = box;
+          }}
+        />
+        <HabeebBox type="saving" onNavigate={this.handleNavigate} />
+        {whereCouldWeSaveBar}
+      </Box>
     );
   }
 }
