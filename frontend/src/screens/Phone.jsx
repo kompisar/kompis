26b1fe6ebf4b8bc72548aaeb3307a163f6c15ac3
Aarni Koskela <akx@iki.fi>
2017-11-26 08:22:16 +0200

    Phone: go to /overview

--- frontend/src/screens/Phone.jsx
+++ frontend/src/screens/Phone.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import {withRouter} from 'react-router-dom';
-import {HotKeys} from 'react-hotkeys';
-import {Flex, Box} from 'reflexbox'
-import {ToastContainer, toast} from 'react-toastify';
+import { withRouter } from 'react-router-dom';
+import { HotKeys } from 'react-hotkeys';
+import { Flex, Box } from 'reflexbox';
+import { ToastContainer, toast } from 'react-toastify';
 import Transition from 'react-transition-group/Transition';
-import {formatEUR} from '../utils';
+import { formatEUR } from '../utils';
 import 'react-toastify/dist/ReactToastify.min.css';
 
-const ZoomInAndOut = ({children, position, ...props}) => (
+const ZoomInAndOut = ({ children, position, ...props }) => (
   <Transition
     {...props}
     timeout={400}
     onEnter={node => node.classList.add('zoom-in', 'animate-notification')}
-    onExit={node => {
+    onExit={(node) => {
       node.classList.remove('zoom-in', 'animate-notification');
       node.classList.add('zoom-out', 'animate-notification');
     }}
@@ -21,17 +21,16 @@ const ZoomInAndOut = ({children, position, ...props}) => (
   </Transition>
 );
 
-const PhoneScreen = withRouter(({history}) => {
-
+const PhoneScreen = withRouter(({ history }) => {
   const createToast = (message, location) => {
     toast(
       (
         <div>
           <Flex>
-            <Box w={1 / 2} style={{textAlign: 'left'}}>Kompis</Box>
-            <Box w={1 / 2} style={{textAlign: 'right'}}>now</Box>
+            <Box w={1 / 2} style={{ textAlign: 'left' }}>Kompis</Box>
+            <Box w={1 / 2} style={{ textAlign: 'right' }}>now</Box>
           </Flex>
-          <div style={{marginTop: '2vh'}}>
+          <div style={{ marginTop: '2vh' }}>
             {message}
           </div>
         </div>
@@ -39,59 +38,60 @@ const PhoneScreen = withRouter(({history}) => {
       {
         closeButton: false,
         onClose:
-          (childrenProps) => history.push(location)
-      }
-    )
+          childrenProps => history.push(location),
+      },
+    );
   };
 
   const keyMap = {
-    'showDemo1': '1',
-    'showDemo2': '2',
-    'showDemo3': '3',
-    'showDemo4': '4',
-    'showDemo5': '5',
+    showDemo1: '1',
+    showDemo2: '2',
+    showDemo3: '3',
+    showDemo4: '4',
+    showDemo5: '5',
   };
 
+  const APP_RETURN_PATH = '/overview';
   const handlers = {
-    'showDemo1': () => createToast(
+    showDemo1: () => createToast(
       <span>
         You just spent {formatEUR(7)} in a bar 🍺
         Have fun!
         You can enjoy up to 2 more drinks tonight.
       </span>,
-      '/result'
+      APP_RETURN_PATH,
     ),
-    'showDemo2': () => createToast(
+    showDemo2: () => createToast(
       <span>
         You have spent {formatEUR(18)} in bars 🍻 tonight.
         If you keep going, you'll go over your non-essential budget! 📉
       </span>,
-      '/result'
+      APP_RETURN_PATH,
     ),
-    'showDemo3': () => createToast(
+    showDemo3: () => createToast(
       <span>
         🎉 PARTY! 🎉
         Yesterday you spent {formatEUR(183.1)} in bars!
         Hope you are feeling alright 🙈 😇
         This affects your saving plan though, should we retune it?
       </span>,
-      '/result'
+      APP_RETURN_PATH,
     ),
-    'showDemo4': () => createToast(
+    showDemo4: () => createToast(
       <span>
         Sees like you just had a dinner for {formatEUR(48.1)},
         should we tune your retirement plan so you can have
         such a dinner once every month? 🍲 🍲 🍲
       </span>,
-      '/result'
+      APP_RETURN_PATH,
     ),
-    'showDemo5': () => createToast(
+    showDemo5: () => createToast(
       <span>
         Good morning!
         It's an exceptional day as you reached 35% of your Vacation Trip goal ✈ 🌴 😎
         Keep up the hard work! 🏋
       </span>,
-      '/result'
+      APP_RETURN_PATH,
     ),
   };
 
@@ -100,7 +100,7 @@ const PhoneScreen = withRouter(({history}) => {
       <ToastContainer
         position="top-center"
         autoClose={9999999999999}
-        hideProgressBar={true}
+        hideProgressBar
         newestOnTop={false}
         transition={ZoomInAndOut}
       />
